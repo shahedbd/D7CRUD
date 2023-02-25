ffe470108c5a045de1bff9de94d9992951e026ce
R M Shahidul Islam <shahedbddev@gmail.com>
2023-02-26 00:04:55 +0600

    + Project code added

---
+++ D7CRUD/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using D7CRUD.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace D7CRUD.Data
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+
+        public DbSet<Category> Category { get; set; }
+    }
+}
---
+++ D7CRUD/Models/Category.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace D7CRUD.Models
+{
+    public class Category
+    {
+        public Int64 Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
